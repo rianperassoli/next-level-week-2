d0647192ef6c4db16a2a9d5f8fac0e50ee00c8a2
Rian Perassoli <rian.perassoli@gmail.com>
2020-08-03 20:55:41 -0300

    Criadas novas tela em branco

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TeacherForm  = () => 
+    <h1>TeacherForm</h1>
+
+
+export default TeacherForm    
---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TeacherList  = () => 
+    <h1>TeacherList</h1>
+
+
+export default TeacherList    
