15786365617c0aa98f97a15db2c21c2d050b8cc4
Rian Perassoli <rian.perassoli@gmail.com>
2020-08-03 20:02:45 -0300

    Configurados estilos globais

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './assets/styles/global.css'
+
 function App() {
   return (
     <div className="App">
-      <h1>Helo</h1>
+      <h1>Hello</h1>
     </div>
   );
 }
