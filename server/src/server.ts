693cce44e20e317cbe4526749454c622b2d2e735
Rian Perassoli <rian.perassoli@gmail.com>
2020-08-04 20:56:24 -0300

    ADicionado e configurado sqlite3 e knex

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,10 @@ import express from 'express'
 
 const app = express()
 
-app.get('/users', () => {
-  console.log('acessou a rota')
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  return response.json({message:'Hello world'})
 })
 
 app.listen(3333)
