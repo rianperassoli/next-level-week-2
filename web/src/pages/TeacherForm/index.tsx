804abb2885145e5081f2f94f3fa3009f5a34a118
Rian Perassoli <rian.perassoli@gmail.com>
2020-08-05 19:54:54 -0300

    Adicionado description ao header

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -4,12 +4,15 @@ import PageHeader from '../../components/PageHeader'
 
 import './styles.css'
 
-const TeacherForm  = () => {
-    return (
-        <div id="page-teacher-form" className="container">
-          <PageHeader title="Que incrível que você quer dar aulas." />
-        </div>
-      )
+const TeacherForm = () => {
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader
+        title="Que incrível que você quer dar aulas."
+        description="O primeiro passo é preencher esse formulário de inscrição"
+      />
+    </div>
+  )
 }
 
 export default TeacherForm    
