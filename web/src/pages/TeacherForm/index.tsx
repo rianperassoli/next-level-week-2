9f51d12438e4cf537cd5a6b37a594c17abae60db
Rian Perassoli <rian.perassoli@gmail.com>
2020-08-05 20:22:20 -0300

    Adicionados inputs ao form de cadastro

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import PageHeader from '../../components/PageHeader'
 
 import './styles.css'
+import Input from '../../components/Input/Input'
 
 const TeacherForm = () => {
   return (
@@ -11,6 +12,17 @@ const TeacherForm = () => {
         title="Que incrível que você quer dar aulas."
         description="O primeiro passo é preencher esse formulário de inscrição"
       />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <Input name="name" label="Nome" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="WhatsApp" />
+
+        </fieldset>
+      </main>
     </div>
   )
 }
