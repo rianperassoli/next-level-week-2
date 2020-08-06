1ccaa20f7e73dbdff74f9392f24944c72532ee9a
Rian Perassoli <rian.perassoli@gmail.com>
2020-08-05 21:24:47 -0300

    Adicionados novos componentes nas telas

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -5,6 +5,7 @@ import PageHeader from '../../components/PageHeader'
 import './styles.css'
 import Input from '../../components/Input'
 import Textarea from '../../components/Textarea'
+import Select from '../../components/Select'
 import warningIcon from '../../assets/images/icons/warning.svg'
 
 const TeacherForm = () => {
@@ -28,7 +29,22 @@ const TeacherForm = () => {
         <fieldset>
           <legend>Sobre a aula</legend>
 
-          <Input name="subject" label="Matéria" />
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Ciências", label: "Ciências" },
+              { value: "Educação física", label: "Educação física" },
+              { value: "Física", label: "Física" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "História", label: "História" },
+              { value: "Matemática", label: "Matemática" },
+              { value: "Português", label: "Português" },
+              { value: "Química", label: "Química" },
+            ]}
+          />
           <Input name="cost" label="Custo da sua hora por aula" />
         </fieldset>
 
