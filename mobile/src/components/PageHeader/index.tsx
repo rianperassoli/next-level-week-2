843afbd56d579075098c1a543ad21ddf8edc8d98
Rian Perassoli <rian.perassoli@gmail.com>
2020-08-06 23:53:26 -0300

    Criado componente PageHeader

---
+++ mobile/src/components/PageHeader/index.tsx
@@ -0,0 +1,39 @@
+
+import React from 'react'
+import { Text, View, Image } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { BorderlessButton } from 'react-native-gesture-handler'
+
+import styles from './styles'
+
+import logoImage from '../../assets/images/logo.png'
+import backIcon from '../../assets/images/icons/back.png'
+
+interface PageHeaderProps {
+  title: string
+}
+
+const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+  const { navigate } = useNavigation()
+
+  function handleNavigateBack() {
+    navigate('Landing')
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topBar}>
+
+        <BorderlessButton onPress={handleNavigateBack}>
+          <Image source={backIcon} resizeMode="contain" />
+        </BorderlessButton>
+
+        <Image source={logoImage} resizeMode="contain" />
+      </View>
+
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  )
+}
+
+export default PageHeader
--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,44 +1,17 @@
 
 import React from 'react'
 import { Text, View, ImageBackground } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
 import { RectButton } from 'react-native-gesture-handler'
+import PageHeader from '../../components/PageHeader'
 
 import styles from './styles'
 
 import giveClassesBGImg from '../../assets/images/give-classes-background.png'
 
 function Favorites() {
-  const { goBack } = useNavigation()
-
-  function handleNavigateBack() {
-    goBack()
-  }
-
   return (
     <View style={styles.container}>
-
-      <ImageBackground
-        resizeMode='contain'
-        style={styles.content}
-        source={giveClassesBGImg}
-      >
-        <Text style={styles.title}>
-          Favorites
-			  </Text>
-
-        <Text style={styles.description}>
-          Para começar, você precisa se cadastrar como professor na nossa plataforma web
-			  </Text>
-
-      </ImageBackground>
-
-      <RectButton
-        style={[styles.okButton]}
-        onPress={handleNavigateBack}
-      >
-        <Text style={styles.okButtonText}>Tudo bem</Text>
-      </RectButton>
+      <PageHeader title="Meus proffys favoritos" />
     </View>
   )
 }
