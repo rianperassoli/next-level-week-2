cf2b00d2431ae35fe1ceeddaf7fc2e04130ef960
Rian Perassoli <rian.perassoli@gmail.com>
2020-08-09 21:15:59 -0300

    Criados filtros de professores

--- mobile/src/components/PageHeader/index.tsx
+++ mobile/src/components/PageHeader/index.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { Text, View, Image } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { BorderlessButton } from 'react-native-gesture-handler'
@@ -11,9 +11,10 @@ import styles from './styles'
 
 interface PageHeaderProps {
   title: string
+  headerRight?: ReactNode
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, headerRight, children }) => {
   const { navigate } = useNavigation()
 
   function handleNavigateBack() {
@@ -31,7 +32,13 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
         <Image source={logoImage} resizeMode="contain" />
       </View>
 
-      <Text style={styles.title}>{title}</Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>{title}</Text>
+
+        {headerRight}
+      </View>
+
+      {children}
     </View>
   )
 }
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,17 +1,55 @@
 
-import React from 'react'
-import { View } from 'react-native'
-import { ScrollView } from 'react-native-gesture-handler'
+import React, { useState } from 'react'
+import { View, Text } from 'react-native'
+import { ScrollView, TextInput, BorderlessButton, RectButton } from 'react-native-gesture-handler'
 import TeacherItem from '../../components/TeacherItem'
+import { Feather } from '@expo/vector-icons'
 
 import PageHeader from '../../components/PageHeader'
 
 import styles from './styles'
 
 function TeacherList() {
+
+  const [isFiltersVisible, setIsFiltersVisible] = useState(false)
+
+
+
   return (
     <View style={styles.container}>
-      <PageHeader title="Proffys disponíveis" />
+      <PageHeader
+        title="Proffys disponíveis"
+        headerRight={(
+          <BorderlessButton
+            onPress={() => setIsFiltersVisible(!isFiltersVisible)}
+          >
+            <Feather name="filter" size={20} color="#FFF" />
+          </BorderlessButton>
+        )}
+      >
+        {isFiltersVisible && (
+          <View style={styles.searchForm}>
+            <Text style={styles.label}>Matéria</Text>
+            <TextInput placeholderTextColor="#c1bccc" style={styles.input} placeholder="Qual a matéria?" />
+
+            <View style={styles.inputGroup}>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Dia da semana</Text>
+                <TextInput placeholderTextColor="#c1bccc" style={styles.input} placeholder="Qual o dia?" />
+              </View>
+
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Horário</Text>
+                <TextInput placeholderTextColor="#c1bccc" style={styles.input} placeholder="Qual o hoário?" />
+              </View>
+            </View>
+
+            <RectButton style={styles.submitButton}>
+              <Text style={styles.submitButtonText}>Filtrar</Text>
+            </RectButton>
+          </View>
+        )}
+      </PageHeader>
 
       <ScrollView
         style={styles.teacherList}
